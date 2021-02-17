29168839415e9251743478ff38f9f208a8702179
Sean <seanh@zillowgroup.com>
2021-02-16 21:26:22 -0800

    workfeb16

--- automation.py
+++ automation.py
@@ -1,6 +1,6 @@
 import os
 from shutil import move
-
+from pathlib import Path
 
 user = os.getenv('USER')
 
@@ -26,24 +26,24 @@ folders_to_create = [root_dir, media_dir,
 
 def create_dir(directories: list):
     # lets create a function that creates files labeled above if they don't already exist
+    if type(directories) != list:
+        raise TypeError('Must be a list!')
     try:
         for folder in directories:
             if not os.path.isdir(folder):
                 os.mkdir(folder)
-
     except OSError as error:
         print(error)
 
 
 # def get_non_hidden_files_except_current_file(root_dir):
-#     return [f for f in os.listdir(root_dir) if os.path.isfile(f) and not f.startswith('.') and not f.__eq__(__file__)]
+#     [f for f in os.listdir(root_dir) if os.path.isfile(f) and not f.startswith('.')]
 
 def get_files(root_dir):
     files = []
     for file in os.listdir(root_dir):
-        print(file)
-        if os.path.isfile(root_dir + file):
-            print('ITS WORKING')
+        if os.path.isfile(root_dir + file) and not file.startswith('.'):
+            # print('ITS WORKING')
             files.append(file)
     return files
 
@@ -52,24 +52,41 @@ def move_files(files):
     for file in files:
         # file moved and overwritten if already exists
         if file.endswith(doc_types):
-            move(file, '{}/{}'.format(documents_dir, file))
-            print('file {} moved to {}'.format(file, documents_dir))
+            if os.path.isfile(documents_dir + file):
+                file = handle_dupe_files(file)
+            move(root_dir + file, documents_dir)
+            print(f'file {file} moved to {documents_dir}')
         elif file.endswith(media_types):
-            move(file, '{}/{}'.format(media_dir, file))
-            print('file {} moved to {}'.format(file, media_dir))
+            move(root_dir + file, media_dir)
+            print(f'file {file} moved to {media_dir}')
         elif file.endswith(software_types):
-            move(file, '{}/{}'.format(software_dir, file))
-            print('file {} moved to {}'.format(file, others_dir))
+            move(root_dir + file, software_dir)
+            print(f'file {file} moved to {software_dir}')
         else:
-            move(file, '{}/{}'.format(others_dir, file))
-            print('file {} moved to {}'.format(file, others_dir))
+            move(root_dir + file, others_dir)
+            print(f'file {file} moved to {others_dir}')
 
+def handle_dupe_files(dir, file):
+    count = 1
+    split_array = file.split('.')
+    begins_with = split_array[0]
+    new_file_name = begins_with + '_' + str(count)
+    while os.path.isfile(dir + new_file_name):
+        count += 1
+    return new_file_name
 
 if __name__ == "__main__":
-    create_dir(folders_to_create)
+    # create_dir(folders_to_create)
     # print(os.listdir(root_dir))
-    files = get_files(root_dir)
-
-    # files = get_non_hidden_files_except_current_file(root_dir)
+    # files = get_files(root_dir)
+    # files = get_files(root_dir)
     # print(files)
-    move_files(files)
+    # move_files(files)
+    test1 = f'/Users/{user}/Downloads/test.txt'
+    # test2 = f'/Users/{user}/Downloads/test.txt'
+    # test3 = f'/Users/{user}/Downloads/test.txt'
+    Path(test1).touch()
+    # Path(test2).touch()
+    # Path(test3).touch()
+    handle_dupe_files(documents_dir, 'test.txt')
+    
--- tests/test_automation.py
+++ tests/test_automation.py
@@ -1,8 +1,8 @@
 import pytest
 import pytest_watch
 import os
-from automation import create_dir, user, folders_to_create
-
+from automation import create_dir, user, folders_to_create, move_files
+from pathlib import Path
 
 def test_does_create_file():
     test = [f'/Users/{user}/Downloads/test']
@@ -10,41 +10,78 @@ def test_does_create_file():
     actual = os.path.isdir(test[0])
     expected = True
     assert actual == expected
-
-
-# def test_raise_error_if_file_exists():
-#     test = f'/Users/{user}/Downloads/test'
-#     create_dir(test)
-#     actual = create_dir(test)
-#     expected = '/Users/benshomefolder/Downloads/test'
-#     assert actual == expected
+    
 
 def test_create_document_folder():
-    test = f'/Users/{user}/Downloads/document'
+    test = [f'/Users/{user}/Downloads/documents']
     create_dir(test)
-    actual = os.path.isdir(test)
+    actual = os.path.isdir(test[0])
     expected = True
     assert actual == expected
 
 
 def test_create_media_folder():
-    test = f'/Users/{user}/Downloads/media'
+    test = [f'/Users/{user}/Downloads/media']
     create_dir(test)
-    actual = os.path.isdir(test)
+    actual = os.path.isdir(test[0])
     expected = True
     assert actual == expected
 
 
 def test_create_software_folder():
-    test = f'/Users/{user}/Downloads/software'
+    test = [f'/Users/{user}/Downloads/software']
     create_dir(test)
-    actual = os.path.isdir(test)
+    actual = os.path.isdir(test[0])
     expected = True
     assert actual == expected
 
 
 def test_create_other_folder():
-    test = f'/Users/{user}/Downloads/others'
-    actual = os.path.isdir(test)
+    test = [f'/Users/{user}/Downloads/others']
+    create_dir(test)
+    actual = os.path.isdir(test[0])
     expected = True
     assert actual == expected
+
+def test_create_dir_is_list():
+    test = f'/Users/{user}/Downloads/others'
+    with pytest.raises(TypeError) as e:
+        create_dir(test)
+    assert str(e.value) == 'Must be a list!'
+
+# test move_files method
+def test_move_file_document():
+    test = f'/Users/{user}/Downloads/test.txt'
+    Path(test).touch()
+    move_files(['test.txt'])
+    actual = os.path.isfile(f'/Users/{user}/Downloads/documents/test.txt')
+    expected = True
+    assert actual == expected 
+    os.remove(f'/Users/{user}/Downloads/documents/test.txt')
+
+def test_move_file_media():
+    test = f'/Users/{user}/Downloads/test.jpg'
+    Path(test).touch()
+    move_files(['test.jpg'])
+    actual = os.path.isfile(f'/Users/{user}/Downloads/media/test.jpg')
+    expected = True
+    assert actual == expected 
+    os.remove(f'/Users/{user}/Downloads/media/test.jpg')
+
+def test_move_file_software():
+    test = f'/Users/{user}/Downloads/test.exe'
+    Path(test).touch()
+    move_files(['test.exe'])
+    actual = os.path.isfile(f'/Users/{user}/Downloads/software/test.exe')
+    expected = True
+    assert actual == expected 
+    os.remove(f'/Users/{user}/Downloads/software/test.exe')
+
+def test_move_file_others(): 
+    test = f'/Users/{user}/Downloads/test.zip'
+    Path(test).touch()
+    move_files(['test.zip'])
+    actual = os.path.isfile(f'/Users/{user}/Downloads/others/test.zip')
+    expected = True
+    assert actual == expected 
+    os.remove(f'/Users/{user}/Downloads/others/test.zip')
